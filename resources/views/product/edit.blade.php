a272d581395486e7126aa5aa5c7205afd736e1b3
Obey72 <4avsemenov@gmail.com>
2018-03-25 22:07:51 +0300

    изменил html - страница редактрования

--- resources/views/product/edit.blade.php
+++ resources/views/product/edit.blade.php
@@ -3,28 +3,27 @@
 @section('title', 'Sneakbox - маркетплейс кроссовок')
 @section('description', 'Sneakbox - Первый маркетплейс в Украине по продаже и покупке кроссовок')
 
-<div class="row create-page" style="padding-bottom: 50px;">
+<div class="row create-page">
 
     <div class="title">Редактировать объявление</div>
 
-    <form action="{{route('product.update', ['alias' => $product->alias])}}" enctype="multipart/form-data"
-          method="post">
+    <form action="{{route('product.update', ['alias' => $product->alias])}}" enctype="multipart/form-data" method="post">
         <div class="col-md-3 info-block">
             <div class="form-group {{$errors->first('title')? 'has-error':''}}">
-                <input class="form-control" type="text" name="title" value="{{$product->title}}"
+                <input type="text" name="title" value="{{$product->title}}"
                        placeholder="Заголовок*">
                 @if ($errors->first('title'))
                     <span class="help-block">{{  $errors->first('title') }}</span>
                 @endif
             </div>
             <div class="form-group {{$errors->first('price')?'has-error':''}}">
-                <input class="form-control" type="text" name="price" value="{{$product->price}}" placeholder="Цена*">
+                <input type="text" name="price" value="{{$product->price}}" placeholder="Цена*">
                 @if ($errors->first('price'))
                     <span class="help-block">{{  $errors->first('price') }}</span>
                 @endif
             </div>
             <div class="form-group {{$errors->first('description')?'has-error':''}}">
-                <textarea class="form-control" type="text" name="description" rows="7"
+                <textarea type="text" name="description" rows="7"
                           placeholder="Описание*">{{$product->description}}</textarea>
                 @if ($errors->first('description'))
                     <span class="help-block">{{$errors->first('description')}}</span>
@@ -32,7 +31,7 @@
             </div>
             <div class="form-group">
                 <label for="category">Категория</label>
-                <select class="form-control" name="category_id" id="category">
+                <select name="category_id" id="category">
                     @foreach(\App\Models\Category::all() as $category)
                         <option value="{{$category->id}}" {{$category->id == $product->category_id ? "selected='selected'":""}}>{{$category->name}}</option>
                     @endforeach
@@ -40,7 +39,7 @@
             </div>
             <div class="form-group">
                 <label for="size">Размер</label>
-                <select class="form-control" name="size" id="size">
+                <select name="size" id="size">
                     @foreach(\App\Models\Size::all() as $size)
                         <option value="{{$size->id}}" {{$size->id == $product->size_id ? "selected='selected'":""}}>{{$size->name}}</option>
                     @endforeach
@@ -48,7 +47,7 @@
             </div>
             <div class="form-group">
                 <label for="condition">Состояние</label>
-                <select class="form-control" name="condition" id="condition">
+                <select name="condition" id="condition">
                     @foreach(\App\Models\Condition::all() as $condition)
                         <option value="{{$condition->id}}" {{$condition->id == $product->condition_id ? "selected='selected'":""}}>{{$condition->name}}</option>
                     @endforeach
@@ -56,10 +55,8 @@
             </div>
 
             <div class="checkbox">
-                <label>
-                    <input type="checkbox" name="checked" value="1" {{$product->active == 1 ? "checked='checked'":""}}>
-                    Опубликовать
-                </label>
+                <label class="sub-title" for="checkbox">Опубликовать</label>
+                <input type="checkbox" name="checked" value="1" {{$product->active == 1 ? "checked='checked'":""}}>
             </div>
             <input type="hidden" name="_token" value="{{Session::token()}}">
             <input class="btn btn-primary" type="submit" value="Сохранить">
@@ -67,7 +64,7 @@
         </div>
 
         <div class="col-md-2 main-image">
-            <label>Главное изображение</label>
+            <div class="sub-title">заглавное фото</div>
             <div id="gallery-image-edit-main" data-img="{{$product->image}}" class="form-group image-item">
                 <div v-if="!image"></div>
                 <div v-else>
